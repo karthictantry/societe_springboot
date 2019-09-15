ac938e18c9554c4560f1ab483947f0d49f47394a
Karthic Tantry <karthic.spikers@live.in>
2019-09-16 02:30:21 +0530

    CORS Update

--- src/main/java/com/employee/controllers/EmployeeRegistrationController.java
+++ src/main/java/com/employee/controllers/EmployeeRegistrationController.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,30 +18,43 @@ import com.employee.beans.Employee;
 import com.employee.constants.CommonConstants;
 import com.employee.validations.Validator;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class EmployeeRegistrationController {
 	
 	private List<Employee> employeeList = new ArrayList<Employee>();
 	
 	@GetMapping(path = "employees", produces = "application/json")
-	public List<Employee> getEmployees() {
-		return employeeList;
+	public ResponseEntity<?> getEmployees() {
+		HttpHeaders headers = new HttpHeaders();
+	    headers.add("Access-Control-Allow-Origin", "*");
+	    headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+		return new ResponseEntity<>(employeeList,headers, HttpStatus.OK);
 	}
 	
 	@PostMapping(path = "employees/register", consumes = "application/json", produces = "application/json")
-	public Map<String, String> addEmployee(@RequestBody Employee employee) {
+	public ResponseEntity<?> addEmployee(@RequestBody Employee employee) {
 		Map<String, String> responseObj = new HashMap<String, String>();
 		Validator validator = new Validator();
+		HttpHeaders headers = new HttpHeaders();
 		if (validator.validateEmployee(employee)) {
 			if (!validator.checkDuplicates(employeeList, employee)) {
 				employeeList.add(employee);
 				responseObj.put(CommonConstants.KEY_REPONSE_CODE, CommonConstants.SUCCESS_REPONSE_CODE);
 				responseObj.put(CommonConstants.KEY_RESPONSE_MESSAGE, CommonConstants.SUCCESS_RESPONSE_MESSAGE);
-				return responseObj;
+				headers.add("Access-Control-Allow-Origin", "*");
+			    headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+			    headers.add("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+			    headers.add("Access-Control-Allow-Credentials", "true");
+			    return new ResponseEntity<>(responseObj, headers,HttpStatus.CREATED);
 			}
 		}
 		responseObj.put(CommonConstants.KEY_REPONSE_CODE, CommonConstants.FAILURE_REPONSE_CODE);
 		responseObj.put(CommonConstants.KEY_RESPONSE_MESSAGE, CommonConstants.FAILURE_RESPONSE_MESSAGE);
-		return responseObj;
+	    headers.add("Access-Control-Allow-Origin", "*");
+	    headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+	    headers.add("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+	    headers.add("Access-Control-Allow-Credentials", "true");
+	    return new ResponseEntity<>(responseObj, headers,HttpStatus.CREATED);
 	}
 }
